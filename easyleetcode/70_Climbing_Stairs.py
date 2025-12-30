196d13adb31f1e15b0c198ee7922892c3ae259bd
NguyenBaNhat <nguyenbanhat090407@gmail.com>
2025-12-30 01:07:39 +0000

    subbmit assignment

---
+++ easyleetcode/70_Climbing_Stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def __init__(self):
+        self.memo={1:1,2:2}
+    def climbStairs(self, n: int) -> int:
+        if n in self.memo:
+            return self.memo[n]
+        a=self.climbStairs(n-1)+self.climbStairs(n-2)
+        self.memo[n]=a
+        return a
